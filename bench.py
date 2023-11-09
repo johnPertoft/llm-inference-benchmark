c3d1d2d2be055f938e12e8eda9e254f692ca37d0
John Pertoft <john.pertoft@gmail.com>
2023-11-09 13:14:57 +0000

    stuff

---
+++ bench.py
@@ -0,0 +1,2 @@
+# TODO: use this to send requests?
+# TODO: Launch from Makefile?
---
+++ frameworks/tensorrt-llm/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# TODO: Make sure we're running it from this directory.
+
+
+# TODO: Take argument for gpu architecture to build for.
+# Docker image build instructions from
+# https://github.com/NVIDIA/TensorRT-LLM/blob/release/0.5.0/docs/source/installation.md
+git clone https://github.com/NVIDIA/TensorRT-LLM.git build/TensorRT-LLM
+cd build/TensorRT-LLM
+git submodule update --init --recursive
+git lfs install
+git lfs pull
+make -C docker release_build CUDA_ARCHS="80-real"
+
+
+# TODO: Tag with cuda arch maybe?
