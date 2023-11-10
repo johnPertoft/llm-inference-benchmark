55212899d74866070de82e5d10840beb62f1d670
John Pertoft <john.pertoft@gmail.com>
2023-11-10 15:16:26 +0000

    wip

--- download.py
+++ download.py
@@ -1,7 +1,9 @@
 from transformers import AutoModelForCausalLM
-from transformers import AutoTokenizer
+from transformers import LlamaTokenizer
 
 p = "meta-llama/Llama-2-7b-chat-hf"
 o = "models/llama-2-7b-chat-hf"
 AutoModelForCausalLM.from_pretrained(p).save_pretrained(o)
-AutoTokenizer.from_pretrained(p).save_pretrained(o)
+LlamaTokenizer.from_pretrained(p, legacy=True).save_pretrained(o)
+# TODO: Using legacy=True is a seems to be necessary for the tensorrt test script to work.
+# Shouldn't be needed if we just load the tokenizer with AutoTokenizer like normally.
--- frameworks/tensorrt-llm/run.sh
+++ frameworks/tensorrt-llm/run.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # TODO: Take argument for which model engine to serve
+# TODO: Switch the entrypoint to run the server.
+# TODO: Maybe add a non root user to the docker image.
 
 docker run -it --rm \
     --gpus all \
     --ipc host \
-    --user $(id -u):$(id -g) \
     --volume /home/john/llm-inference-benchmark/models:/models \
     --volume /home/john/llm-inference-benchmark/frameworks/tensorrt-llm/engines:/engines \
-    --entrypoint bash \
-    tensorrt-llm-server
-
-#python examples/llama/summarize.py --test_trt_llm --hf_model_location /models/llama-2-7b-chat-hf/ --data_type fp16 --engine_dir ./tmp/llama/7B/trt_engines/fp16/1-gpu/ --test_hf
+    tensorrt-llm-server \
+    python tensorrt_llm/examples/llama/summarize.py \
+        --test_trt_llm \
+        --hf_model_location /models/llama-2-7b-chat-hf/ \
+        --data_type fp16 \
+        --engine_dir /engines/fp16/1-gpu/ \
+        --test_hf
