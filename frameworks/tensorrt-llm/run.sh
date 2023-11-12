19e42512a2c90df30cae493b89b1a31bb344a5cc
John Pertoft <john.pertoft@gmail.com>
2023-11-12 20:56:47 +0000

    wip

--- frameworks/tensorrt-llm/run.sh
+++ frameworks/tensorrt-llm/run.sh
@@ -10,10 +10,17 @@ docker run -it --rm \
     --volume /home/john/llm-inference-benchmark/models:/models \
     --volume /home/john/llm-inference-benchmark/frameworks/tensorrt-llm/engines:/engines \
     tensorrt-llm-server \
-    python tensorrt_llm/examples/llama/run.py \
-        --engine_dir /engines/fp16/1-gpu/ \
-        --tokenizer_dir /models/llama-2-7b-chat-hf/ \
-        --max_output_len 1024
+    python benchmarks/python/benchmark.py \
+        -m llama_13b \
+        --mode plugin \
+        --batch_size "64" \
+        --input_output_len "256,128" \
+        --warm_up 10
+    # python tensorrt_llm/examples/llama/run.py \
+    #     --engine_dir /engines/fp16/1-gpu/ \
+    #     --tokenizer_dir /models/llama-2-7b-chat-hf/ \
+    #     --input_text "Write python code for merge sort and add some tests for it" \
+    #     --max_output_len 2048
     # python tensorrt_llm/examples/llama/summarize.py \
     #     --test_trt_llm \
     #     --hf_model_location /models/llama-2-7b-chat-hf/ \
