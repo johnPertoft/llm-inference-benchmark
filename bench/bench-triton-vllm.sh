77adba56f37440f12e9247fb0e3c06c40c160379
John Pertoft <john.pertoft@gmail.com>
2023-11-26 21:29:34 +0000

    wip

--- bench/bench-triton-vllm.sh
+++ bench/bench-triton-vllm.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 set -e
+
+locust_file="$(dirname $0)/locust-triton-vllm.py"
+locust --headless \
+    --locustfile "${locust_file}" \
+    --host http://localhost:8000/v2/models/vllm_model \
+    --users 10 \
+    --spawn-rate 10 \
+    --run-time 3m
--- frameworks/fastapi-hf-transformers/build-image.sh
+++
@@ -1,2 +0,0 @@
-# TODO: Just plain huggingface transormers.
-# TODO: Maybe with what quantization is supported.
