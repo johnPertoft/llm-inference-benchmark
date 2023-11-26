911223157e93440fd3ffadd158098f7cf83baf72
John Pertoft <john.pertoft@gmail.com>
2023-11-26 21:24:25 +0000

    wip

--- frameworks/triton-tensorrt-llm/test.sh
+++ frameworks/triton-tensorrt-llm/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-curl -X POST http://localhost:8000/v2/models/vllm_model/generate -d \
+curl -X POST http://localhost:8000/v2/models/ensemble/generate -d \
 '{
     "text_input": "[INST] How do I count to thirty in German? [/INST]",
     "parameters": {
-        "max_tokens": 100,
-        "stream": false,
-        "temperature": 0
+        "max_tokens": 128,
+        "bad_words": [""],
+        "stop_words": [""]
     }
 }' | jq .text_output
--- test-tgi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-curl 127.0.0.1:8000/generate \
-    -X POST \
-    -d '{"inputs":"[INST] How do I count to thirty in German? [/INST]","parameters":{"max_new_tokens": 256}}' \
-    -H 'Content-Type: application/json'
